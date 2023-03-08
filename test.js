0807272f54b74095e0c2791753ba304c3f5d7de7
KarimovMurodilla <karimovmurodilla15@gmail.com>
2023-03-08 22:47:13 +0500

    some changes

--- test.js
+++ test.js
@@ -1,37 +1,39 @@
 let tg = window.Telegram.WebApp;
 
 tg.expand();
+tg.MainButton.textColor = "#FFFFFF";
+tg.MainButton.color = "#2cab37";
 
 
 const quantityBlocks = document.querySelectorAll('.quantity');
 quantityBlocks.forEach(function (block) {
-  const minusButton = block.querySelector('.minus');
-  const plusButton = block.querySelector('.plus');
-  const quantityInput = block.querySelector('input[type="text"]');
+    const minusButton = block.querySelector('.minus');
+    const plusButton = block.querySelector('.plus');
+    const quantityInput = block.querySelector('input[type="text"]');
 
-  // обработчик клика по кнопке уменьшения
-  minusButton.addEventListener('click', function () {
-    const currentValue = parseFloat(quantityInput.value); // текущее значение в поле ввода
-    if (currentValue == 0) { // проверяем на минимальное значение
-        tg.MainButton.hide();
-    }
-  });
+    // обработчик клика по кнопке уменьшения
+    minusButton.addEventListener('click', function () {
+        const currentValue = parseFloat(quantityInput.value); // текущее значение в поле ввода
+        if (currentValue == 0) { // проверяем на минимальное значение
+            tg.MainButton.hide();
+        }
+    });
 
-  // обработчик клика по кнопке увеличения
-  plusButton.addEventListener('click', function () {
-    tg.MainButton.setText("Вы выбрали товар!");
-    tg.MainButton.show();
-  });
+    // обработчик клика по кнопке увеличения
+    plusButton.addEventListener('click', function () {
+        tg.MainButton.setText("Вы выбрали товар!");
+        tg.MainButton.show();
+    });
 
 
 
-//   // обработчик изменения значения в поле ввода количества
-//   quantityInput.addEventListener('change', function () {
-//     const currentValue = parseFloat(quantityInput.value); // текущее значение в поле ввода
-//     if (currentValue < parseInt(quantityInput.min)) { // проверяем на минимальное значение
-//       quantityInput.value = quantityInput.min;
-//     } else if (currentValue > parseInt(quantityInput.max)) { // проверяем на максимальное значение
-//       quantityInput.value = quantityInput.max;
-//     }
-//   });
+    //   // обработчик изменения значения в поле ввода количества
+    //   quantityInput.addEventListener('change', function () {
+    //     const currentValue = parseFloat(quantityInput.value); // текущее значение в поле ввода
+    //     if (currentValue < parseInt(quantityInput.min)) { // проверяем на минимальное значение
+    //       quantityInput.value = quantityInput.min;
+    //     } else if (currentValue > parseInt(quantityInput.max)) { // проверяем на максимальное значение
+    //       quantityInput.value = quantityInput.max;
+    //     }
+    //   });
 });
