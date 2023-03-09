6c838227031717be5cbd482394ab2ba2f63374b2
KarimovMurodilla <karimovmurodilla15@gmail.com>
2023-03-09 12:38:38 +0500

    bug fix

--- test.js
+++ test.js
@@ -1,41 +1,33 @@
 let tg = window.Telegram.WebApp;
 
-tg.expand();
-tg.MainButton.textColor = "#FFFFFF";
-tg.MainButton.color = "#2cab37";
+// wait for Telegram UI to load
+Telegram.WebApp.onLoaded(function() {
+  tg.expand();
 
+  // set MainButton color and text color
+  tg.MainButton.textColor = "#FFFFFF";
+  tg.MainButton.color = "#2cab37";
 
-const quantityBlocks = document.querySelectorAll('.quantity');
-quantityBlocks.forEach(function (block) {
-    const minusButton = block.querySelector('.minus');
-    const plusButton = block.querySelector('.plus');
-    const quantityInput = block.querySelector('input[type="text"]');
+  const quantityBlocks = document.querySelectorAll('.quantity');
+  quantityBlocks.forEach(function (block) {
+      const minusButton = block.querySelector('.minus');
+      const plusButton = block.querySelector('.plus');
+      const quantityInput = block.querySelector('input[type="text"]');
 
-    // // обработчик клика по кнопке уменьшения
-    // minusButton.addEventListener('click', function () {
-    //     const currentValue = parseFloat(quantityInput.value); // текущее значение в поле ввода
-    //     if (currentValue == 0) { // проверяем на минимальное значение
-    //         tg.MainButton.hide();
-    //     }
-    // });
+      // обработчик клика по кнопке увеличения
+      plusButton.addEventListener('click', function () {
+          tg.MainButton.setText("Вы выбрали товар!");
+          tg.MainButton.show();
+      });
+  });
 
-    // обработчик клика по кнопке увеличения
-    plusButton.addEventListener('click', function () {
-        tg.MainButton.setText("Вы выбрали товар!");
-        tg.MainButton.show();
-    });
+  // send data when MainButton is clicked
+  Telegram.WebApp.onEvent("mainButtonClicked", function(){
+      tg.sendData(item);
+  });
+
+  let usercard = document.getElementById("usercard");
+  let p = document.createElement("p");
+  p.innerText = `${tg.initDataUnsafe.user.first_name} ${tg.initDataUnsafe.user.last_name}`;
+  usercard.appendChild(p);
 });
-
-Telegram.WebApp.onEvent("mainButtonClicked", function(){
-	tg.sendData(item);
-});
-
-let usercard = document.getElementById("usercard");
-
-let p = document.createElement("p");
-
-p.innerText = `${tg.initDataUnsafe.user.first_name}
-${tg.initDataUnsafe.user.last_name}`;
-
-
-usercard.appendChild(p);
